2d499a9cf2521d2b97552988f30f7989e7bb4247
Eky <eky@ekyso.com>
2026-08-17 22:35:16 -0400

    Version bump to 4.5.2.6

--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -82,7 +82,7 @@ internal static class EarlyConstants
     internal static int? LogScreenId { get; set; }
 
     /// <summary>SMAPI's current raw semantic version.</summary>
-    internal static string RawApiVersion = "4.5.2.5";
+    internal static string RawApiVersion = "4.5.2.6";
 
     /*********
     ** Private methods
